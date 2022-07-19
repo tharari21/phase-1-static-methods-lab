0718786f62111e292ab0e837889e835a63036bc3
Tomer Harari <tharari21@gmail.com>
2022-07-19 14:00:35 -0400

    commit tree dc885d9dcf3dd0f2bb2bcd88185c0f658f6b8162

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.slice(0, 1).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, "");
+  }
+  static titleize(string) {
+    const capitalize = (str) => {
+      if (
+        !["an", "a", "but", "of", "and", "for", "at", "by", "from"].includes(
+          str
+        )
+      ) {
+        return Formatter.capitalize(string);
+      }
+      return string;
+    };
+    return string.split(" ").map(capitalize).join(" ");
+  }
 }
